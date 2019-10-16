9a751aef8171804d3dffd72ecaee54d33a30249d
yuanjunling <261412489@qq.com>
2019-10-16 11:04:53 +0800

    绑定book路由

--- Django_DouBan/simple_api_server_test/get_by_douban/urls.py
+++ Django_DouBan/simple_api_server_test/get_by_douban/urls.py
@@ -1,7 +1,9 @@
 #coding:utf-8
 from django.urls import path
-from .views import Music,Movie
+from .views import Music,Movie,Book
 urlpatterns = [
     path('music/',Music.as_view()),
-    path('movie/',Movie.as_view())
+    path('movie/',Movie.as_view()),
+    path('book/',Book.as_view())
+
 ]
