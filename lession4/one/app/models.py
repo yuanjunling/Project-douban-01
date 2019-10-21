8b555585e809625f6b3a8a711932e8191c2a5f37
yuanjunling <261412489@qq.com>
2019-10-21 17:23:57 +0800

    models

--- lession4/one/app/models.py
+++ lession4/one/app/models.py
@@ -6,7 +6,7 @@ class Test(models.Model):
     name = models.CharField(max_length=20)
 
 class User(models.Model):
-    id = models.IntegerField(primary_key=True)
+
     username = models.CharField(max_length=50,blank=False,unique=True)
     age = models.SmallIntegerField(default=0)
     phone = models.IntegerField(db_index=True,blank=True,default=0,max_length=11)
@@ -17,21 +17,25 @@ class User(models.Model):
 
     class Meta:#索引
         index_together = ['username','phone']
-
+    def __str__(self):
+        return 'User:{}'.format(self.username)
 
 class Userprofile(models.Model):
-    id = models.IntegerField(primary_key=True)
+
     user = models.OneToOneField(User,blank=True,null=True,on_delete=models.SET_NULL)
     birthday = models.CharField(max_length=100,blank=True,default='')
+    def __str__(self):
+        return 'user:{},birthday:{}'.format(self.user.username,self.birthday)
 
 class Diary(models.Model):
-    id = models.IntegerField(primary_key=True)
+
     user = models.ForeignKey(User,related_name='diary',on_delete=models.SET_NULL,blank=True,null=True)
     content = models.TextField(max_length=800)
     create_time = models.IntegerField()
 
 class Group(models.Model):
-    id = models.IntegerField(primary_key=True)
+
     user = models.ManyToManyField(User,related_name='group')
     name = models.CharField(max_length=20)
     create_time = models.IntegerField()
+
